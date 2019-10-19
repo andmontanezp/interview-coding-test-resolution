510a20a4d63ccb3a010b71501325ecd9d07ae73d
Anderson Montañez <assembler07@gmail.com>
2019-10-19 12:26:59 -0300

    Covering first case, both sellIn and price properties should lowers after update price process - RED

---
+++ src/test/java/com/anderson/testco/CarInsuranceTest.java
@@ -0,0 +1,33 @@
+package com.anderson.testco;
+
+import com.anderson.testco.entities.Product;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CarInsuranceTest {
+
+    private CarInsurance carInsurance;
+
+    @Before public void setUp() {
+    }
+
+    @Test
+    public void givenProductList_ByEOD_SellInAndPriceShouldLowers() {
+        carInsurance = new CarInsurance(new Product[]{
+                new Product("Medium Coverage", 10, 20),
+                new Product("Medium Coverage", 20, 10)
+        });
+
+        Product[] products = carInsurance.updatePrice();
+        assertThat(products.length, is(2));
+        assertThat(products[0].getSellIn(), is(9));
+        assertThat(products[0].getPrice(), is(19));
+    }
+}
